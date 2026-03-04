c94a108178ee87692cef8f5d6a4628e167ffd675
MartinezDelAlamoMarco <147182879+MartinezDelAlamoMarco@users.noreply.github.com>
2026-03-04 16:22:45 +0100

    Conectada base de datos de prueba

--- backend/app/Http/Controllers/VehiculoController.php
+++ backend/app/Http/Controllers/VehiculoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Vehicle;
 use Illuminate\Http\Request;
 
 class VehiculoController extends Controller
@@ -10,10 +11,13 @@ class VehiculoController extends Controller
     public function index()
     {
         // Datos de ejemplo
+        /*
         $vehiculos = [
             ['id' => 1, 'marca' => 'Toyota', 'modelo' => 'Corolla', 'precio' => 15000],
             ['id' => 2, 'marca' => 'Honda', 'modelo' => 'Civic', 'precio' => 17000],
         ];
+        */
+        $vehiculos = Vehicle::all();
 
         return response()->json($vehiculos);
     }
