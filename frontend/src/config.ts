854b253a8b802e1a2ad405dd86c579c9dc494ac7
MartinezDelAlamoMarco <147182879+MartinezDelAlamoMarco@users.noreply.github.com>
2026-03-05 12:29:23 +0100

    Cambiado título y arreglada conexión a API desde el front

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 // src/config.ts
-//export const API_BASE_URL: string = 'https://tfg-backend-2fcr.onrender.com/api';
-export const API_BASE_URL: string = 'http://127.0.0.1:8000/api';
+export const API_BASE_URL: string = 'https://tfg-backend-2fcr.onrender.com/api';
+//export const API_BASE_URL: string = 'http://127.0.0.1:8000/api';
