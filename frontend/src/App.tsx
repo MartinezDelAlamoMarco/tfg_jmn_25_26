fd94c944fe33f31b6735ab9b146d062c4361a4b7
MartinezDelAlamoMarco <147182879+MartinezDelAlamoMarco@users.noreply.github.com>
2026-03-05 16:31:52 +0100

    TailwindCSS instalado correctamente y hecha alguna prueba

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -61,29 +61,24 @@ function App() {
 
   return (
     <>
-      <h1>REDLINE MOTORS</h1>
+      <h1 className="text-5xl font-bold underline mb-5">REDLINE MOTORS</h1>
 
-      <button onClick={handleGetVehiculos}>Obtener todos</button>
-      <p>
-        <input
-          type="text"
-          placeholder="Id"
-          value={searchId}
-          onChange={(e) => setSearchId(e.target.value)}
-        />
-        <button onClick={handleGetVehiculo}>Buscar</button>
-      </p>
-
+      <button className="rounded-full" onClick={handleGetVehiculos}>Obtener todos</button>
+        <div className="max-w-xl mx-auto p-6">
+            <label className="text-sm font-medium">ID</label>
+            <div className="flex items-center mt-1">
+                <input type="text" className="w-full h-10 px-3 text-sm border border-r-0 rounded-r-none border-blue-500 focus:outline-none rounded shadow-sm" placeholder="Example: 1" value={searchId} onChange={(e) => setSearchId(e.target.value)}/>
+                <button className="h-10 px-4 text-sm bg-blue-500 border border-l-0 border-blue-500 rounded-r shadow-sm text-blue-50 hover:text-white hover:bg-blue-400 hover:border-blue-400 focus:outline-none" onClick={handleGetVehiculo}>Buscar</button>
+            </div>
+        </div>
       <div>
-        <h2>Vehículos</h2>
-
-        {loading && <p>Cargando vehículos...</p>} {/* ✅ mostrar mientras carga */}
+        <h2 className="text-3xl font-bold underline mb-2">Vehículos</h2>
+        {loading && <p>Cargando vehículos...</p>}{" "}
+        {/* mostrar mientras carga */}
         {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-
         {!loading && !errorMessage && vehiculos.length === 0 && (
           <p>No hay vehículos disponibles</p>
         )}
-
         {!loading && vehiculos.length > 0 && (
           <ul>
             {vehiculos.map((v) => (
