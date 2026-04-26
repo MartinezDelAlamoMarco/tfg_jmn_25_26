2a646901d98a994098e515f7e2201892e8365be3
Javibu29 <jpinelalcaraz@gmail.com>
2026-04-26 11:40:53 +0200

    Actualizando cambios

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -2,42 +2,54 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import MainLayout from "../layouts/MainLayout"
 
+// Páginas de Núcleo (Públicas)
 import Home from "../pages/nucleo/Home"
 import Login from "../pages/nucleo/Login"
 import Register from "../pages/nucleo/Register"
 import PasswordRecovery from "../pages/nucleo/PasswordRecovery"
 import CreateProfile from "../pages/nucleo/CreateProfile"
 import VehicleDetail from "../pages/nucleo/VehicleDetail" 
-// 1. IMPORTAMOS LA NUEVA PANTALLA DE ALQUILER
-import AlquilerScreen from "../pages/nucleo/AlquilerScreen" 
 
-import AdvertisementsScreen from "../pages/gestionVendedor/AdvertisementsScreen" 
-import CreateAd from "../pages/gestionVendedor/CreateAd";
-import EditAd from '../pages/gestionVendedor/EditAd';
+// Páginas de Alquiler (Nuevas)
+import AlquilerScreen from "../pages/nucleo/AlquilerScreen"
 import AlquilerDetailScreen from "../pages/nucleo/AlquilerDetailScreen"
+
+// Páginas de Gestión (Privadas / Vendedor)
+import AdvertisementsScreen from "../pages/gestionVendedor/AdvertisementsScreen" 
+import CreateAd from "../pages/gestionVendedor/CreateAd"
+import EditAd from '../pages/gestionVendedor/EditAd'
+import CreateRent from "../pages/gestionVendedor/CreateAdRent" // <--- IMPORTANTE
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
 
+        {/* Todas las rutas dentro de MainLayout compartirán el mismo Navbar y Footer */}
         <Route element={<MainLayout />}>
 
+          {/* Inicio y Auth */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/password-recovery" element={<PasswordRecovery />} />
           <Route path="/create-profile" element={<CreateProfile />} />
-          <Route path="/advertisement/:id" element={<VehicleDetail />} />
-          
-          {/* 2. AÑADIMOS LA RUTA DE ALQUILER */}
-          {/* Usamos :id para saber qué coche se quiere alquilar al entrar */}
-          <Route path="/alquileres" element={<AlquilerScreen />} />
-          <Route path="/alquiler/:id" element={<AlquilerDetailScreen />} />
 
+          {/* Flujo de Compra/Venta */}
+          <Route path="/advertisement/:id" element={<VehicleDetail />} />
           <Route path="/mis-anuncios" element={<AdvertisementsScreen />} />
           <Route path="/create-ad" element={<CreateAd />} />
           <Route path="/editar-anuncio/:id" element={<EditAd />} />
+
+          {/* Flujo de Alquiler (TFG) */}
+          {/* 1. Catálogo de alquileres */}
+          <Route path="/alquileres" element={<AlquilerScreen />} />
+          
+          {/* 2. Vista única de reserva de un coche */}
+          <Route path="/alquiler/:id" element={<AlquilerDetailScreen />} />
+          
+          {/* 3. Formulario para que el vendedor publique un coche para alquilar */}
+          <Route path="/create-rent" element={<CreateRent />} />
 
         </Route>
 
